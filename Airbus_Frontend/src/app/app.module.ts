7bae56be687f698b52c068ff6dfc21e34b879264
Arvind <arvind.n228@gmail.com>
2024-01-16 17:04:18 +0530

    Arvind's frontend commit

--- Airbus_Frontend/src/app/app.module.ts
+++ Airbus_Frontend/src/app/app.module.ts
@@ -3,18 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
+import { UserHomeComponent } from './user-home/user-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminHomeComponent,
+    AdminLoginComponent,
+    UserLoginComponent,
+    UserRegisterComponent,
+    UserHomeComponent,
     
     
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      {path : 'admin', component : AdminHomeComponent}
+      {path : 'admin', component : AdminHomeComponent},
+      {path : 'admin-login', component : AdminLoginComponent},
+      {path : 'user-login', component : UserLoginComponent},
+      {path : 'user-register', component : UserRegisterComponent}
     ])
   ],
   providers: [],
